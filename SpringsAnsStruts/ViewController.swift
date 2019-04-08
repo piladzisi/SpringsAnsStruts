cc3d53b55d5946673c2c5347b70958ac3a9091cf
Anna <piladzisi@gmail.com>
2019-04-08 18:29:35 +0300

    Initial Commit

---
+++ SpringsAnsStruts/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  SpringsAnsStruts
+//
+//  Created by anna.sibirtseva on 08/04/2019.
+//  Copyright © 2019 anna.sibirtseva. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var redView: UIView!
+    @IBOutlet weak var greenView: UIView!
+    
+    @IBOutlet weak var blueView: UIView!
+    @IBOutlet weak var yellowView: UIView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
