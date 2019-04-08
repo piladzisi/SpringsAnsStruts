a2b48e1f437b70488cba800a5fe12974782f4d53
Anna <piladzisi@gmail.com>
2019-04-08 18:43:33 +0300

    hard coding suck

--- SpringsAnsStruts/ViewController.swift
+++ SpringsAnsStruts/ViewController.swift
@@ -20,7 +20,22 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
-
+    override func viewWillLayoutSubviews() {
+        super.viewWillLayoutSubviews()
+        
+        let orientation = UIDevice.current.orientation
+        if orientation == .landscapeLeft || orientation == .landscapeRight {
+            redView.frame = CGRect(x: 20, y: 20, width: 307, height: 161.5)
+            greenView.frame = CGRect(x: 340, y: 20, width: 307, height: 161.5)
+            yellowView.frame = CGRect(x: 20, y: 193, width: 307, height: 161.5)
+            blueView.frame = CGRect(x: 340, y: 193, width: 307, height: 161.5)
+        } else {
+            redView.frame = CGRect(x: 25, y: 44, width: 160, height: 300)
+            greenView.frame = CGRect(x: 191, y: 44, width: 160, height: 300)
+            yellowView.frame = CGRect(x: 25, y: 348, width: 160, height: 300)
+            blueView.frame = CGRect(x: 191, y: 348, width: 160, height: 300)
+            
+        }
+    }
 }
 
